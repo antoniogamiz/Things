9ddfd4e25460fe40e6ada0b023e7ef89d5fd63d0
Antonio <antoniogamiz10@gmail.com>
2019-10-01 13:23:07 +0200

    setup file

---
+++ install.sh
@@ -0,0 +1,48 @@
+#!/bin/bash 
+
+# ================= confiuration vars =======================
+
+TMP_DIR=/tmp/antonio
+
+# ================= helper functions ========================
+
+function DEBUG {
+    echo -e "\e[32m========> $1 \e[0m"
+}
+
+function ERROR {
+    echo -e "\e[31m [ERROR] $1 \e[0m"
+}
+
+# ===========================================================
+
+DEBUG "Computer setup"
+
+if [ -d $TMP_DIR ];
+then
+    ERROR "$TMP_DIR already exists. Please remove it"
+    ERROR "or change TMP_DIR configuration variable."
+    exit 1
+else
+    DEBUG "$TMP_DIR used for tmp files"
+    mkdir $TMP_DIR
+    cd $TMP_DIR
+fi
+# ===========================================================
+
+DEBUG "Visual Studio Code"
+sudo apt install code
+
+# ===========================================================
+
+DEBUG "Python related"
+sudo apt install python -y
+
+# ===========================================================
+
+DEBUG "Latex related" 
+sudo apt install texlive-base
+sudo apt install textlive-latex-recommended
+sudo apt install texlive
+sudo apt install texlive-latex-extra
+sudo apt install texlive-full
